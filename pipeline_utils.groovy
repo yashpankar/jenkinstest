8d25ec3c9679f2e607192e2e92ab7cddf54aef8e
yashpankar <153274846+yashpankar@users.noreply.github.com>
2024-08-29 15:09:03 +0530

    Update pipeline_utils.groovy

--- pipeline_utils.groovy
+++ pipeline_utils.groovy
@@ -2,10 +2,6 @@ def cleanWorkspace() {
     sh '''
         rm -f .gitinfo
         rm -rf build dist *.egg-info
-        find "${TEST_PROJECT_SOURCE_FOLDER}" -name __pycache__ | xargs rm -rf
-        find "${TEST_PROJECT_TESTS_FOLDER}" -name __pycache__ | xargs rm -rf
-        find . -name .pytest_cache | xargs rm -rf
-        find "${TEST_PROJECT_SOURCE_FOLDER}" -name "*.pyc" -delete
         rm -rf .coverage
         rm -rf "tune_results/*"
     '''
