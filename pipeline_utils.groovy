a7eec50721c6c65334cea25f43966c92918e8e4c
yashpankar <153274846+yashpankar@users.noreply.github.com>
2024-08-29 15:38:04 +0530

    Update pipeline_utils.groovy

--- pipeline_utils.groovy
+++ pipeline_utils.groovy
@@ -4,6 +4,10 @@ def cleanWorkspace() {
         rm -rf build dist *.egg-info
         rm -rf .coverage
         rm -rf "tune_results/*"
+        find . -name __pycache__ | xargs rm -rf
+        find . -name __pycache__ | xargs rm -rf
+        find . -name .pytest_cache | xargs rm -rf
+        find . -name "*.pyc" -delete
     '''
 }
 
