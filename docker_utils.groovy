6ad54769c121a4098693725f74652117632f0893
yashpankar <153274846+yashpankar@users.noreply.github.com>
2024-09-02 13:18:04 +0530

    Update docker_utils.groovy

--- docker_utils.groovy
+++ docker_utils.groovy
@@ -1,29 +1,11 @@
+
 def buildDockerImage() {
      echo "docker build"
 }
 
-def pushDockerImage(String imageName) {
-    sh """
-        docker --tlsverify \
-            --tlscacert \$DOCKER_TLS_CLIENTDIR/ca.pem \
-            --tlscert \$DOCKER_TLS_CLIENTDIR/cert.pem \
-            --tlskey \$DOCKER_TLS_CLIENTDIR/key.pem \
-            push ${imageName}
-    """
-}
-
-def runDockerContainer(String imageName, String command) {
-    sh """
-        docker --tlsverify \
-            --tlscacert \$DOCKER_TLS_CLIENTDIR/ca.pem \
-            --tlscert \$DOCKER_TLS_CLIENTDIR/cert.pem \
-            --tlskey \$DOCKER_TLS_CLIENTDIR/key.pem \
-            run --rm ${imageName} ${command}
-    """
-}
-
 def dockerLogin() {
-       echo "docker login"
+     echo "docker login"
 }
+
 
 return this
